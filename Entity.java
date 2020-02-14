80ce8379a626a186faf80b1917fbf860ef30f040
SmallBirb <59892961+SmallBirb@users.noreply.github.com>
2020-02-14 16:17:56 -0700

    Update Entity.java
    Added constructor and methods (addAttack, reduceAttack, setAttack, getAttack, setName, getName, toString)

--- Entity.java
+++ Entity.java
@@ -1,4 +1,34 @@
 
 public class Entity {
-  System.out.println("Bruh");
+  private String name;
+	private int attack;
+	
+	public Entity(String nama, int atk) {
+		name = nama;
+		attack = atk;
+	}
+	
+	public void addAttack(int addition) {
+		attack += addition;
+	}
+	public void reduceAttack(int reduction) {
+		attack -= reduction;
+	}
+	public void setAttack(int toSet) {
+		attack = toSet;
+	}
+	public int getAttack() {
+		int attackCopy = attack;
+		return attackCopy;
+	}
+	public void setName(String noime) {
+		name = noime;
+	}
+  public String getName() {
+		String nameCopy = name;
+		return nameCopy;
+	}
+	public String toString() {
+		return name + " has an attack of " + attack
+	}
 }

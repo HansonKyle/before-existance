e83e1008bd6a6830f58eff158462716cd9f88dba
Anil Mawji <mawjianil1@gmail.com>
2020-02-20 17:51:59 -0700

    Updated working tree.

--- before-existance/src/Attack.java
+++ before-existance/src/Attack.java
@@ -1,20 +1,25 @@
 public class Attack {
-
-	private int damage;
-
-	public Attack(int damage) {
-		this.damage = damage;
-	}
+	// Two entities one being the target of the attack and one being the attacker or source of damage
+	private Entity target;
+	private Entity attacker;
 	
-	public Attack(Attack attack) {
-		this.damage = attack.damage;
+	public Attack(Entity target, Entity attacker) {
+		this.target = target;
+		this.attacker = attacker;
 	}
-	
-	public int getDamage() {
-		return damage;
+	public Entity getTarget() {
+		return target;
 	}
-
-	public void setDamage(int damage) {
-		this.damage = damage;
+	public void setTarget(Entity target) {
+		this.target = target;
+	}
+	public Entity getattacker() {
+		return attacker;
+	}
+	public void setattacker(Entity attacker) {
+		this.attacker = attacker;
+	}
+	public void attacking(Entity attacker, Entity target) {
+		target.setHealth(target.getHealth() - attacker.getCurrentWeapon().getDamage());
 	}
 }
--- before-existance/src/Weapon.java
+++ before-existance/src/Weapon.java
@@ -1,25 +1,9 @@
-public class Weapon {
-	
-	private String name;
-	int damage;
-	
-	public Weapon(String name, int damage) {
-		this.name = name;
-		this.damage = damage;
-	}
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+public class Weapon extends Equippable{
+	private int damage;
 
 	public int getDamage() {
 		return damage;
 	}
-
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}

21da998ba5bb279b96b036010411be3710a8b435
SmallBirb <59892961+SmallBirb@users.noreply.github.com>
2020-02-22 12:34:15 -0700

    Update Story.java

--- before-existance/src/Story.java
+++ before-existance/src/Story.java
@@ -1,36 +1,62 @@
+import java.util.Scanner;
 public class Story {
-	
+
 	private int numOfEvents;
-	
+
 	public Story(int events) {
 		numOfEvents = events;
 	}
+	public void playerInitialize() {
+		System.out.println("Enter your name: ");
+		Scanner keyboard = new Scanner(System.in);
+		Player you = new Player(keyboard.nextLine(), 100);
+		System.out.println(you.toString());
+	}
 	
 	public void nextRoom() {
-		double randomNumber = Math.random();
-		if (randomNumber <= 0.2) {
-			// generate certain type of room
-		} else if (randomNumber <= 0.4) {
+		double randomRoom = Math.random();
+		double randomChance = Math.random();
+		if (randomRoom <= 0.2) {
+			Event second = new Event(null, "Nothing");
+			second.addChoice(new Choice(1, "Go through door") {
+				public void activate() {
+					if (randomChance <= 0.15) {
+						System.out.println("You pushed when you should have pulled and walked into the door. \n(-10 Health)");
+						//you.removeHealth(10);
+					} else {
+						System.out.println("You open the door and walk into the next room.");
+					}
+				}
+			});
+		} else if (randomRoom <= 0.4) {
 			// generate other type of room
-		} else if (randomNumber <= 0.6) {
+		} else if (randomRoom <= 0.6) {
 			// generate different type of room
-		} else if (randomNumber <= 0.8) {
+		} else if (randomRoom <= 0.8) {
 			// generate big room
 		} else {
 			// generate small room
 		}
 	}
-	
+
+	public void start() {
+		Room startingRoom = new Room(5, 5);
+		System.out.println(startingRoom.toString());
+		Event first = new Event(null, "Nothing");
+		//Menu startingMenu = new Menu(this);
+		first.addChoice(new Choice(1, "Go through door") {
+			public void activate() {
+					System.out.println("You open the door and walk into the next room.");
+			}
+		});
+		for (int i = 0; i < numOfEvents; i++) {
+			this.nextRoom();
+		}
+	}
+
 	@Override
 	public String toString() {
 		String statement = "Number of events: " + numOfEvents;
 		return statement;
 	}
-	
-	//main function used to test this class
-	/*
-	public static void main(String[] args) {
-		Story yayStoryTime = new Story(10);
-		System.out.println(yayStoryTime);
-	} */
 }

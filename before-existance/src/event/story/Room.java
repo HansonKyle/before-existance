a38962a560b4f9fe788728b80aedf42bbe06d0b3
SmallBirb <59892961+SmallBirb@users.noreply.github.com>
2020-02-25 18:38:24 -0700

    Added comments

--- before-existance/src/event/story/Room.java
+++ before-existance/src/event/story/Room.java
@@ -1,15 +1,16 @@
 package event.story;
+// Room class generates room sizes for events
 public class Room {
 	
 	private int roomLength;
 	private int roomWidth;
 
-	public Room(int l, int w) {
+	public Room(int l, int w) { // General constructor
 		roomLength = l;
 		roomWidth = w;
 	}
 
-	public Room() {
+	public Room() { // Random constructor
 		int roomAdjustmentConstant = 20; // Arbitrary until room size needs to be converted into pixels
 		double randomNumberLength = Math.random();
 		double randomNumberWidth = Math.random();

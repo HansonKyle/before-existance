461b78e2809435004d8d910726cfdd9d2055822c
SmallBirb <59892961+SmallBirb@users.noreply.github.com>
2020-02-25 18:36:49 -0700

    Added comments

--- before-existance/src/event/story/Story.java
+++ before-existance/src/event/story/Story.java
@@ -3,7 +3,7 @@ package event.story;
 import core.Game;
 import event.Event;
 import event.EventGenerator;
-
+// Story class stores events in an arrayList and prints events for player to read and respond to. Also initiates player and prints end of game statements.
 public class Story {
 
 	private final EventGenerator eventGenerator;
@@ -11,7 +11,7 @@ public class Story {
 	private Game game;
 	private Event[] events;
 
-	public Story(Game game, int numEvents) {
+	public Story(Game game, int numEvents) { // Generates list of events using eventGenerator class
 		this.game = game;
 		this.events = new Event[numEvents];
 		this.eventGenerator = new EventGenerator(game);
@@ -22,19 +22,19 @@ public class Story {
 	}
 
 	public void start() {
-		if (game.getPlayer() == null) {
+		if (game.getPlayer() == null) { // initializes player if player does not exist
 			game.initPlayer();
 		}
 		
 		int i = 0;
-		while (i < events.length && game.getPlayer().isAlive()) {
+		while (i < events.length && game.getPlayer().isAlive()) { // displays and requests input for every event in arrayList
 			System.out.println(events[i].getDescription());
 			events[i].displayChoices();
 			events[i].requestChoice();
 			System.out.println();
 			i++;
 		}
-		if (game.getPlayer().isAlive()) {
+		if (game.getPlayer().isAlive()) { // prints end of game statements depending if player is alive or not
 			game.getPlayer().dungeonSurvived();
 			System.out.println("----------------------------------");
 			System.out.println("You have completed this dungeon. Congratulations!");

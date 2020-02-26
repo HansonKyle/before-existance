0fea4a0fc4b9c8db3febb5aac97dfb1e47ad545f
SmallBirb <59892961+SmallBirb@users.noreply.github.com>
2020-02-25 17:14:36 -0700

    Added comments

--- before-existance/src/event/Choice.java
+++ before-existance/src/event/Choice.java
@@ -1,20 +1,21 @@
 package event;
+// Choice class stores options for the player to execute in the form of a key binding with a description
 public abstract class Choice {
 	
 	private char keyBinding;
 	private String description;
 
-	public Choice(char keyBinding, String description) {
+	public Choice(char keyBinding, String description) { // General constructor
 		this.keyBinding = keyBinding;
 		this.description = description;
 	}
 	
-	public Choice(Choice choice) {
+	public Choice(Choice choice) { // Copy constructor
 		this.keyBinding = choice.keyBinding;
 		this.description = choice.description;
 	}
 	
-	public abstract void activate();
+	public abstract void activate(); // Abstract activate method that allows for different choices to have different activation methods
 	
 	@Override
 	public String toString() {

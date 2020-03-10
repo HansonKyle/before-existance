ddfc40148321972eb98fd813ca3f011b4c87fedc
SmallBirb <59892961+SmallBirb@users.noreply.github.com>
2020-03-10 13:57:52 -0600

    Add files via upload
    Update main menu for iteration 2

---
+++ before-existance/src/core/HowToPlayController.java
@@ -0,0 +1,32 @@
+package core;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class HowToPlayController {
+
+	@FXML
+	private Button returnToMainButton;
+
+	@FXML
+	//This method will change the scene to the How To Play scene when called
+	public void howToPlayButtonPressed (ActionEvent event) throws IOException {	
+		Parent howToPlayParent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+		Scene howToPlayScene = new Scene(howToPlayParent);
+		
+		//This line gets stage info
+		Stage window = (Stage)(((Node)event.getSource()).getScene().getWindow());
+		howToPlayScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		window.setScene(howToPlayScene);
+		window.show();	
+		}
+	
+}

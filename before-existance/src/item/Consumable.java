96990bf39f34ce0c9f4cbb1a95e5573a3088f536
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2020-04-15 16:16:12 -0600

    Update Consumable.java

--- before-existance/src/item/Consumable.java
+++ before-existance/src/item/Consumable.java
@@ -1,10 +1,12 @@
 package item;
 public class Consumable extends Item {
 	
+	//Give name to the consumable
 	public Consumable(String name) {
 		super(name);
 	}
-
+	
+	//Use the consumable
 	@Override
 	public void use() {
 		

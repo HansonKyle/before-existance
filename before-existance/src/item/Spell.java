9610f4dfc8808319a3f0da41f5ca00c391b117b1
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2020-04-15 16:21:44 -0600

    Update Spell.java

--- before-existance/src/item/Spell.java
+++ before-existance/src/item/Spell.java
@@ -1,12 +1,15 @@
 package item;
 public class Spell {
 	
+	//Set a variable for the spell name
 	private String name;
 	
+	//Get the spell name
 	public String getName() {
 		return name;
 	}
 	
+	//Set the spell name
 	public void setName(String name) {
 		this.name = name;
 	}

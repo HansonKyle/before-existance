da9cbc7ae6c78aacd51d1c08536ebdd041ddfb07
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2020-04-15 16:20:47 -0600

    Update Item.java

--- before-existance/src/item/Item.java
+++ before-existance/src/item/Item.java
@@ -1,18 +1,23 @@
 package item;
 public abstract class Item {
 	
+	//Set a variable for the name
 	private String name;
 	
+	//Apply the name
 	public Item(String name) {
 		this.name = name;
 	}
 	
+	//Use the method use
 	public abstract void use();
 	
+	//Get the name
 	public String getName() {
 		return name;
 	}
 	
+	//Set the name
 	public void setName(String name) {
 		this.name = name;
 	}

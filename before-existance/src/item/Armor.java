7c32d3bdd5fb1ea61e947f34fd6765dc9b281d99
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2020-04-15 16:13:40 -0600

    Update Armor.java

--- before-existance/src/item/Armor.java
+++ before-existance/src/item/Armor.java
@@ -1,17 +1,21 @@
 package item;
 public class Armor extends Equippable {
 	
+	//Set variable for armour health
 	private int healthIncrease;
 	
+	//Give health to the specific armour piece
 	public Armor(String name, int healthIncrease) {
 		super(name);
 		this.healthIncrease = healthIncrease;
 	}
-
+	
+	//Get the armours health
 	public int getHealthIncrease() {
 		return healthIncrease;
 	}
-
+	
+	//Set the armours health
 	public void setHealthIncrease(int healthIncrease) {
 		this.healthIncrease = healthIncrease;
 	}

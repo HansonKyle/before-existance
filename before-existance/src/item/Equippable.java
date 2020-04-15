9c4aba701bf61b4a5e54b15bbf473f99714a8e9f
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2020-04-15 16:18:39 -0600

    Update Equippable.java

--- before-existance/src/item/Equippable.java
+++ before-existance/src/item/Equippable.java
@@ -1,20 +1,25 @@
 package item;
 public class Equippable extends Item {
 	
+	//Get name of the equippable
 	public Equippable(String name) {
 		super(name);
 	}
-
+	
+	//Set the state of equipState to false
 	private Boolean equipState = false;
 	
+	//Set the equipeState to true
 	public void equipItem() {
 		equipState = true;
 	}
 	
+	//Set the equipeState to false
 	public void unequipItem() {
 		equipState = false;
 	}
 	
+	//Get the equipeState
 	public Boolean getEquipState() {
 		return equipState;
 	}

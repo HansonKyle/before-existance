f39a2a7f4df4a3dbab5f078f5d8622c6a447f04a
HarprabhGill <60674278+HarprabhGill@users.noreply.github.com>
2020-04-15 16:23:21 -0600

    Update Weapon.java

--- before-existance/src/item/Weapon.java
+++ before-existance/src/item/Weapon.java
@@ -1,17 +1,21 @@
 package item;
 public class Weapon extends Equippable {
 	
+	//Create a variable for damage
 	private int damage;
 	
+	//Apply the name and damage
 	public Weapon(String name, int damage) {
 		super(name);
 		this.damage = damage;
 	}
-
+	
+	//Get the damage
 	public int getDamage() {
 		return damage;
 	}
 	
+	//Set the damage
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
